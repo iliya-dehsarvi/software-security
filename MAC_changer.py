b3b85fd09f7270105f4e1ad47a38bd01fb7e533e
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-07 15:15:57 -0800

    Update MAC_changer.py

--- MAC_changer.py
+++ MAC_changer.py
@@ -4,9 +4,13 @@ import optparse
 
 
 def get_args():
-    parser = optparse.OptionParser()
-    parser.add_option('-i', '--interface', dest='interface', help='Interface t0 CHANGE its mac address')
-    parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
+    try:
+        parser = optparse.OptionParser()
+        parser.add_option('-i', '--interface', dest='interface', help='Interface t0 CHANGE its mac address')
+        parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
+    except Exception as err: 
+        print(err)
+        exit(1)
     return parser.parse_args()
 
 
