55bb768362ef47a0d5d074c055b767e0d1f9005e
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-07 15:20:39 -0800

    Update MAC_changer.py

--- MAC_changer.py
+++ MAC_changer.py
@@ -4,15 +4,13 @@ import optparse
 
 
 def get_args():
-    try:
-        parser = optparse.OptionParser()
-        parser.add_option('-i', '--interface', dest='interface', help='Interface t0 CHANGE its mac address')
-        parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
-    except Exception as err: 
-        print('[-] ' + err)
-        exit(1)
-    return parser.parse_args()
-
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--interface', dest='interface', help='Interface t0 CHANGE its mac address')
+    parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
+    options, args = parser.parse_args()
+    if not options.interface: raise Exception('[-] missing interface argument')
+    if not options.new_mac: raise Exception('[-] missing MAC address argument')
+    return options, args
 
 def change_mac(interface, new_mac):
     print('[+] Changing interface for ' + interface + ' to ' + new_mac)
