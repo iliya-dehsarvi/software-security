380728563f2b6dcbd39de8d857b99266d005b956
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-07 14:15:19 -0800

    Update MAC_changer.py

--- MAC_changer.py
+++ MAC_changer.py
@@ -2,5 +2,5 @@
 import subprocess
 subprocess.call('ifconfig', shell=True)
 subprocess.call('ifconfig eth0 down', shell=True)
-subprocess.call('ifconfig eth0 hw ether 00:11:22:33:44:66', shell=True)
+subprocess.call('ifconfig hw eth0 ether 00:11:22:33:44:66', shell=True)
 subprocess.call('ifconfig up', shell=True)
