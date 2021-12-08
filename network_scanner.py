dbf7d9568a4871603e1ff4d168dabfe8a2ee2cb1
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-08 13:22:24 -0800

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -6,8 +6,8 @@ def scan(IP):
     arp_request = scapy.ARP(pdst=IP)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')   
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    answered, _ = scapy.srp(arp_request_broadcast, timeout=1)
     print(answered.summary())
 
 if __name__ == '__main__': 
-    scan('192.168.89.133.1/24')
+    scan('10.20.3.134.1/24')
