7310ef5e5cb06a49910b82e00584a959735f50ee
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-12 12:29:18 -0800

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,7 +4,7 @@ import scapy.all as scapy
 from scapy.layers import http
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
 
 def process_sniffed_packet(packet):
     if packet.hasLayer(http.HTTPRequest):
