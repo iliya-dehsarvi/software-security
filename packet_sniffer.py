c662cb7df6095b199ed717bc821f99b0cf436338
iliya52 <Dehsarvi.iliya@gmail.com>
2021-12-14 23:24:28 -0800

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,13 +1,13 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 
-# import scapy.all as scapy
-# from scapy.layers import http
+import scapy.all as scapy
+from scapy.layers import http
 
-# def sniff(interface):
-#     scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
 
-# def process_sniffed_packet(packet):
-#     if packet.hasLayer(http.HTTPRequest):
-#         print(packet.show())
+def process_sniffed_packet(packet):
+    if packet.hasLayer(http.HTTPRequest):
+        print(packet.show())
 
-# sniff('eth0')
+sniff('eth0')
